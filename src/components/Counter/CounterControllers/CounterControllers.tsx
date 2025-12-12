6f92f4370c9d71686b6a5e73077a7fd31ec77f43
Volodymyr Pashchenko <volodkaposhta@gmail.com>
2025-12-12 11:42:08 +0200

    update

--- src/components/Counter/CounterControllers/CounterControllers.tsx
+++ src/components/Counter/CounterControllers/CounterControllers.tsx
@@ -9,6 +9,8 @@ const CounterControllers = ({
   increment,
   decrement,
 }: CounterControllersProps) => {
+  const x = 10;
+
   return (
     <div className={css["counterControllers"]}>
       <button onClick={decrement}>Decrement</button>
